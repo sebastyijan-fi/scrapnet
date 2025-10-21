fb7a73e1685c8705eba9433033c5590999a4cb5c
sebastyijan-fi <westinni@arcada.fi>
2025-10-21 09:24:29 +0000

    nuked

--- website/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},    // ← the new Tailwind PostCSS plugin
-    autoprefixer: {},              // ← still handle vendor prefixes
-  },
-};
